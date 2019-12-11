dd564b603e85b6ccf42578451d700cd5591c1f7e
Joel Stockamp <stockampj@gmail.com>
2019-12-10 22:21:44 -0800

    start a fetch request with a header

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,25 @@ export const changeState = (value1) => ({
   type: types.CHANGE_STATE,
   value1
 });
+
+let requestHeader = {
+  headers: {
+    'x-api-key': 'bd67d9ad9aca4084a9794f755888ae6c'
+  }
+}
+
+export function fetchPlayerMembershipId(userName) {
+  return function (dispatch) {
+    const localRequestId = v4();
+    userName = userName.replace(' ', '_');
+    return fetch(`https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/-1/${userName}/`, requestHeader).then(
+      response => response.json(),
+      error => console.log('An error occurred.', error)
+    ).then(function(json) {
+      console.log('CHECK OUT THIS SWEET API RESPONSE:', json)
+    });   
+  };
+}
+
+
+
